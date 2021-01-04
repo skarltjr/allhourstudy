20b7d99f7d81bd812bc7932421805d5eb0956621
kiseok <kisa0828@naver.com>
2021-01-05 02:23:23 +0900

    리소스에 대해서도 securityconfig설정

--- src/main/java/com/allhour/allhourstudy/infra/config/SecurityConfig.java
+++ src/main/java/com/allhour/allhourstudy/infra/config/SecurityConfig.java
@@ -1,11 +1,15 @@
 package com.allhour.allhourstudy.infra.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
@@ -16,6 +20,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         "/email-login", "/login-by-email", "/search/study").permitAll()
                 .mvcMatchers(HttpMethod.GET, "/profile/*").permitAll()
                 .anyRequest().authenticated();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception
+    {
+        web.ignoring()
+                .mvcMatchers("/node_modules/**")
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
 }
